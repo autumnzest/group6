be9d0eac5d0d081365cd2de5f3a7f0edf0f93a89
root <root@ip-172-31-21-40.ap-northeast-1.compute.internal>
2017-12-02 00:03:38 +0000

    css commit

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if($_GET['action'] == "logout"){
-    session_destroy();
-    echo 'logout success！click <a href="userlogin.html">登録</a>';
-    exit;
-}
-
-?>
--- tmp/test.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$test = "aws s3 cp 11.txt s3://data.aiit-cdp-10.work";
-$last = shell_exec($test);
-print "last: $last\n";
-?>
